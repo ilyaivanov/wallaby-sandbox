d86d8131e95f29f59986a6d806534e08e7fb40d5
Ilya Ivanov <static.ila@gmail.com>
2015-08-20 13:36:44 +0300

    Wallaby configuration

---
+++ app/spec.js
@@ -0,0 +1,10 @@
+describe("when calling add function", function () {
+    it("result should return the sum of two input parameters", function () {
+        expect(add(2,3)).toBe(5);
+    });
+
+    it("failing test", function () {
+        expect(1).toBe(2);
+    });
+
+});
